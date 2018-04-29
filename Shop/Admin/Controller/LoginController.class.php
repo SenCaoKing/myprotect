95db39085968f3da5be2fb95130ad477f6eef6f8
SenCaoKing <1134349927@qq.com>
2018-04-30 01:07:51 +0800

    新增后台登录页

---
+++ Shop/Admin/Controller/LoginController.class.php
@@ -0,0 +1,13 @@
+<?php
+namespace Admin\Controller;
+use Think\Controller;
+/**
+ * 后台登录页控制器
+ */
+class LoginController extends Controller {
+    public function login(){
+        $this->display();
+    }
+	
+	
+}
---
+++ Shop/Runtime/Cache/Home/21e4f08d0b94dfc424d54041c36d1dd1.php
@@ -0,0 +1,1 @@
+<?php if (!defined('THINK_PATH')) exit();?>前台模板还未完成
