c1d93f398986b8cebbb97aa1067ee4da45b4f792
SenCaoKing <1134349927@qq.com>
2018-05-01 02:37:15 +0800

    管理员增删改查

--- Shop/Admin/Controller/AdminController.class.php
+++ Shop/Admin/Controller/AdminController.class.php
@@ -1,19 +1,57 @@
 <?php
 namespace Admin\Controller;
 class AdminController extends BaseController{
-
-
 	public function lst(){
+        $model = D('Admin/Admin');
+        $data = $model->search();
+        $this->assign(array(
+            'data' => $data['data'],
+            'page' => $data['page'],
+        ));
 		$this->display();
 	}
 
 	public function add(){
+        if(IS_POST){
+            $model = D('Admin/Admin');
+            if($model->create(I('post.'), 1)){
+                if($id = $model->add()){
+                    $this->success('添加成功', U('lst?p='.I('get.p')));
+                    exit;
+                }
+            }
+            $this->error($model->getError());
+        }
 		$this->display();
 	}
 
 	public function edit(){
+        $id = I('get.id');
+        if(IS_POST){
+            $model = D('Admin/Admin');
+            if($model->create(I('post.'), 2)){
+                if($id = $model->save() !== FALSE){
+                    $this->success('修改成功', U('lst'， array('p'=>I('get.p', 0))));
+                    exit;
+                }
+            }
+            $this->error($model->getError());
+        }
+        $model = M('Admin');
+        $data = $model->find($id);
+        $this->assign('data', $data);
 		$this->display();
 	}
+
+    public function delete(){
+        $model = D('Admin/Admin');
+        if($model->delete(I('get.id', 0)) !== FALSE){
+            $this->success('删除成功！', U('lst', array('p'=>I('get.p'))));
+            exit;
+        } else {
+            $this->error($model->getError());
+        }
+    }
 
 
 
