31bdcf09b73aca69425daf5ab9816aa1b9bfe94f
SenCaoKing <1134349927@qq.com>
2018-05-03 22:48:58 +0800

    修改角色列表

--- Shop/Runtime/Cache/Admin/43f8f52d0c757010faf47a4e9b15b56f.php
+++ Shop/Runtime/Cache/Admin/43f8f52d0c757010faf47a4e9b15b56f.php
@@ -3,7 +3,7 @@
 <head>
     <!--标题-->
     
-    <title>管理中心 - 角色列表</title>
+    <title>管理中心 - 权限列表</title>
 
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <link href="/Public/Admin/Styles/general.css" rel="stylesheet" type="text/css" />
@@ -14,7 +14,6 @@
     
     <style type="text/css">
         td{text-align: center;}
-        a{color: #9cf !important;}
     </style>
 
 </head>
@@ -24,8 +23,8 @@
 
     <!--具体操作-->
     
-    <span class="action-span"><a href="<?php echo U('add');?>">添加角色</a></span>
-    <span id="search_id"> - 角色列表</span>
+    <span class="action-span"><a href="<?php echo U('add');?>">添加权限</a></span>
+    <span id="search_id"> - 权限列表</span>
 
 
     <div style="clear:both;"></div>
@@ -36,20 +35,29 @@
     <div class="list-div" id="listDiv">
         <table cellspacing="1" cellpadding="3">
             <tr>
-                <th>角色ID</th>
-                <th>角色名称</th>
-                <th>拥有的权限</th>
+                <th>ID</th>
+                <th>权限名称</th>
+                <th>模块名称</th>
+                <th>控制器名称</th>
+                <th>方法名称</th>
+                <th>上级权限的ID，0：代表顶级权限</th>
                 <th width="60">操作</th>
             </tr>
-            <?php if(is_array($data)): $i = 0; $__LIST__ = $data;if( count($__LIST__)==0 ) : echo "" ;else: foreach($__LIST__ as $key=>$v): $mod = ($i % 2 );++$i;?><tr class="tron">
+            <?php foreach($data as $k => $v): ?>
+                <tr class="tron">
                     <td><?php echo ($v['id']); ?></td>
-                    <td><?php echo ($v['role_name']); ?></td>
-                    <td><?php echo ($v['auth_names']); ?></td>
+                    <td style="text-align: left;text-indent: 4px;">
+                        <?php echo str_repeat('-', 8*$v['auth_level']); echo $v['auth_name']; ?></td>
+                    <td><?php echo $v['module_name']; ?></td>
+                    <td><?php echo $v['controller_name']; ?></td>
+                    <td><?php echo $v['action_name']; ?></td>
+                    <td><?php echo $v['pid']; ?></td>
                     <td align="center">
-                        <a href="<?php echo U('edit?id='.$v['id']); ?>" title="编辑">编辑</a> |
-                        <a href="<?php echo U('delete?id='.$v['id']); ?>" onclick="return confirm('确定要删除吗？');" title="移除">移除</a>
+                        <a href="<?php echo U('edit?id='.$v['id'].'&p='.I('get.p')); ?>" title="编辑" style="color:#9cf;">编辑</a> |
+                        <a href="<?php echo U('delete?id='.$v['id'].'&p='.I('get.p')); ?>" onclick="return confirm('确定要删除吗？');" title="移除" style="color:#9cf;">移除</a>
                     </td>
-                </tr><?php endforeach; endif; else: echo "" ;endif; ?>
+                </tr>
+            <?php endforeach; ?>
         </table>
     </div>
 
