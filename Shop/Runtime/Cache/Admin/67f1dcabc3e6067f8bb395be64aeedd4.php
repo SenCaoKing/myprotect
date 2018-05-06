4be1c0d85f5c11b19a583000aab30547edff4370
SenCaoKing <1134349927@qq.com>
2018-05-06 09:49:45 +0800

    各列表页样式修改

--- Shop/Runtime/Cache/Admin/67f1dcabc3e6067f8bb395be64aeedd4.php
+++ Shop/Runtime/Cache/Admin/67f1dcabc3e6067f8bb395be64aeedd4.php
@@ -1,18 +1,20 @@
-<extend name="Common:common" />
-<!-- 标题 -->
-<block name="title">
+<?php if (!defined('THINK_PATH')) exit();?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <!--标题-->
+    
     <title>管理中心 - 商品列表</title>
-</block>
-<!-- 具体操作 -->
-<block name="action">
-    <span class="action-span"><a href="{:U('add')}">添加商品</a></span>
-    <span id="search_id"> - 商品列表</span>
-</block>
-<!-- 其他样式 -->
-<block name="asset">
-<link rel="stylesheet" type="text/css" href="__PUBLIC__/datepicker/jquery-ui-1.9.2.custom.min.css" />
-<script type="text/javascript" charset="utf-8" src="__PUBLIC__/datepicker/jquery-1.7.2.min.js"></script>
-<script type="text/javascript" charset="utf-8" src="__PUBLIC__/datepicker/jquery-ui-1.9.2.custom.min.js"></script>
+
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <link href="/Public/Admin/Styles/general.css" rel="stylesheet" type="text/css" />
+    <link href="/Public/Admin/Styles/main.css" rel="stylesheet" type="text/css" />
+    <link href="/Public/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <script type="text/javascript" src="/Public/bootstrap/js/jquery.min.js"></script>
+    <!-- 其他样式 -->
+    
+<link rel="stylesheet" type="text/css" href="/Public/datepicker/jquery-ui-1.9.2.custom.min.css" />
+<script type="text/javascript" charset="utf-8" src="/Public/datepicker/jquery-1.7.2.min.js"></script>
+<script type="text/javascript" charset="utf-8" src="/Public/datepicker/jquery-ui-1.9.2.custom.min.js"></script>
 <style type="text/css">
     input{margin: 5px !important}
     td{text-align: center;}
@@ -28,16 +30,29 @@
         $('#end_addtime').datepicker({dateFormat:"yy-mm-dd"});
     });
 </script>
-</block>
+
+</head>
+<body>
+<h1 style="font-size: 14px;">
+    <span><a href="<?php echo U('Index/main');?>" style="color:#9cf;">管理中心</a></span>
+
+    <!--具体操作-->
+    
+    <span class="action-span"><a href="<?php echo U('add');?>">添加商品</a></span>
+    <span id="search_id"> - 商品列表</span>
+
+
+    <div style="clear:both;"></div>
+</h1>
 
 <!-- 内容主题 -->
-<block name="main">
+
     <div class="form-div">
         <form>
             <input type="hidden" name="p" value="1" />
-            商品名称：<input type="text" name="goods_name" value="{:I('get.goods_name')}"/><br/>
-             价　　格：<input type="text" name="start_price" value="{:I('get.start_price')}"/>~
-             <input type="text" name="end_price" value="{:I('get.end_price')}" /><br/>
+            商品名称：<input type="text" name="goods_name" value="<?php echo I('get.goods_name');?>"/><br/>
+             价　　格：<input type="text" name="start_price" value="<?php echo I('get.start_price');?>"/>~
+             <input type="text" name="end_price" value="<?php echo I('get.end_price');?>" /><br/>
             是否上架：<input type="radio" name="is_on_sale" value="-1" <?php if(I('get.is_on_sale', -1) == -1) echo 'checked="checked"'; ?> />全部
             <input type="radio" name="is_on_sale" value="1" <?php if(I('get.is_on_sale', -1) == 1) echo 'checked="checked"'; ?> />是
             <input type="radio" name="is_on_sale" value="0" <?php if(I('get.is_on_sale', -1) == 0) echo 'checked="checked"'; ?> />否<br />     
@@ -45,7 +60,7 @@
             <input type="radio" name="is_delete" value="1" <?php if(I('get.is_delete', -1) == 1) echo 'checked="checked"'; ?> />是
             <input type="radio" name="is_delete" value="0" <?php if(I('get.is_delete', -1) == 0) echo 'checked="checked"'; ?> />否 <br />
             添加时间：
-            <input type="text" size="10" id="start_addtime" name="start_addtime" value="{:I('get.start_addtime')}"/>~<input type="text" size="10" id="end_addtime" name="end_addtime" value="{:I('get.end_addtime')}"/><br/>
+            <input type="text" size="10" id="start_addtime" name="start_addtime" value="<?php echo I('get.start_addtime');?>"/>~<input type="text" size="10" id="end_addtime" name="end_addtime" value="<?php echo I('get.end_addtime');?>"/><br/>
             <input type="submit" class="btn btn-info" value="搜索"/><br /><br />     
             排序方式：
             <input onclick="parentNode.submit();" type="radio" name="odby" value="id_asc" <?php if(I('get.odby', 'id_asc') == 'id_asc') echo 'checked="checked"'; ?> />根据添加时间升序
@@ -69,23 +84,25 @@
                 <th>是否删除</th>
                 <th>操作</th>
             </tr>
-            <foreach name="data" item="v">
-                <tr>
-                    <td>{$v.id}</td>
-                    <td>{$v.addtime|date="Y-m-d H:i:s",###}</td>
-                    <td>{$v.goods_name}</td>
-                    <td><img src="UPLOAD_PATH<?php echo substr($v['sm_logo'],16); ?>" /></td>
-                    <td>{$v.shop_price}</td>
-                    <td>{$v.goods_desc}</td>
-                    <td><if condition="$v.is_on_sale eq 1">上架<else/>下架</if></td>
-                    <td><if condition="$v.is_delete eq 1">是<else/>否</if></td>
+            <?php if(is_array($data)): foreach($data as $key=>$v): ?><tr>
+                    <td><?php echo ($v["id"]); ?></td>
+                    <td><?php echo (date("Y-m-d H:i:s",$v["addtime"])); ?></td>
+                    <td><?php echo ($v["goods_name"]); ?></td>
+                    <td><img src="/PUBLIC/Uploads<?php echo substr($v['sm_logo'],16); ?>" /></td>
+                    <td><?php echo ($v["shop_price"]); ?></td>
+                    <td><?php echo ($v["goods_desc"]); ?></td>
+                    <td><?php if($v["is_on_sale"] == 1): ?>上架<?php else: ?>下架<?php endif; ?></td>
+                    <td><?php if($v["is_delete"] == 1): ?>是<?php else: ?>否<?php endif; ?></td>
                     <td>
-                    <a href="{:U('edit',array('id'=>$v['id'],'p'=>I('get.p',1)))}" class="action">修改</a>
-                    <a onclick="return confirm('确定要删除吗?')" href="{:U('delete',array('id'=>$v['id'],'p'=>I('get.p',1)))}" class="action">删除</a>
+                    <a href="<?php echo U('edit',array('id'=>$v['id'],'p'=>I('get.p',1)));?>" class="action">修改</a>
+                    <a onclick="return confirm('确定要删除吗?')" href="<?php echo U('delete',array('id'=>$v['id'],'p'=>I('get.p',1)));?>" class="action">删除</a>
                     </td>
-                </tr>
-            </foreach>
-            <tr><td colspan="9">{$page}</td></tr>
+                </tr><?php endforeach; endif; ?>
+            <tr><td colspan="9"><?php echo ($page); ?></td></tr>
         </table>
     </div>
-</block>
+
+
+<div id="footer">版权所有，侵权必究@2017</div>
+</body>
+</html>
